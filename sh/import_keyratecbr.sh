f879f76d694d4c4184d76421f59956ea9b560294
Anton Kokorin <ant.kkrn@gmail.com>
2023-09-19 15:14:47 +0000

    Fix import tables

--- sh/import_keyratecbr.sh
+++ sh/import_keyratecbr.sh
@@ -2,7 +2,6 @@
 
 source ../.env
 CONTAINER_NAME="help_ddu_bot-db-1"
-VOLUME_PATH="/var/lib/docker/volumes/help_ddu_bot_pg_data/_data/"
+VOLUME_PATH="/var/lib/postgresql/data"
 sudo cp ../data/keyratecbr.csv $VOLUME_PATH
-sudo docker exec $CONTAINER_NAME psql -U $POSTGRES_USER -d $POSTGRES_DB CREATE TABLE IF NOT EXIST keyratecbr (id SERIAL PRIMARY KEY, date DATE NOT NULL, rate REAL NOT NULL);
--c "\COPY keyratecbr FROM '/var/lib/postgresql/data//keyratecbr.csv' CSV HEADER"
+sudo docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -d $POSTGRES_DB -c "\COPY keyratecbr FROM '$VOLUME_PATH/keyratecbr.csv' CSV HEADER"
