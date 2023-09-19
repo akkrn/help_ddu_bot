9f4088937b264af9a3ac434e8e0a6ace9227028c
Anton Kokorin <ant.kkrn@gmail.com>
2023-09-19 13:51:13 +0000

    to local

--- sh/import_keyratecbr.sh
+++ sh/import_keyratecbr.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 source ../.env
-
-psql -h DB_HOST -U $POSTGRES_USER -d $POSTGRES_DB -c "\COPY keyratecbr FROM '../data/keyratecbr.csv' CSV HEADER"
+CONTAINER_NAME="help_ddu_bot-db-1"
+VOLUME_PATH="/var/lib/docker/volumes/help_ddu_bot_pg_data/_data/"
+sudo cp ../data/keyratecbr.csv $VOLUME_PATH
+sudo docker exec $CONTAINER_NAME psql -U $POSTGRES_USER -d $POSTGRES_DB CREATE TABLE IF NOT EXIST keyratecbr (id SERIAL PRIMARY KEY, date DATE NOT NULL, rate REAL NOT NULL);
+-c "\COPY keyratecbr FROM '/var/lib/postgresql/data//keyratecbr.csv' CSV HEADER"
